bfe4a4449e81fc812e9d2b84f4b6ea5fccf6f03d
sergey <sergey.s.maslov@gmail.com>
2020-05-04 00:52:51 +0300

    initial commit (only test)

--- quadratic.py
+++ quadratic.py
@@ -7,5 +7,5 @@ c = float(sys.argv[2])
 
 discriminant = b * b - 4.0 * c
 d =math.sqrt(discriminant)
-stdio.writeln((-b + d) / 2.0)
-stdio.writeln((-b - d) / 2.0)
+print((-b + d) / 2.0)
+print((-b - d) / 2.0)
